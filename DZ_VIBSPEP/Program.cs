4e1249bb95e5e6751e465126adfa6d0664c55175
Anton <115809418+KlyuchkaAnton@users.noreply.github.com>
2023-03-05 22:04:35 +0300

    Написал програмный код согласно ТЗ

--- DZ_VIBSPEP/Program.cs
+++ DZ_VIBSPEP/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество элементов массива:");
+
+int AmAl = int.Parse(Console.ReadLine()!);
+
+string[] words;
+
+words = new string[AmAl];
+//////////////////////////////////////////////////////////////////////////
+Console.WriteLine($"Введите {AmAl} элемента массива words");
+
+for  (int i = 0; i < words.Length; i++)
+{
+    words[i] = Console.ReadLine()!;
+}
+
+Console.Write(" words [ ");
+
+for  (int i = 0; i < words.Length; i++)
+{
+Console.Write($"{words[i]} ");
+}
+Console.Write("]");
+Console.WriteLine();
+
+ //////////////////////////////////////////////////////////////////////////
+string[] NS = new string[AmAl];
+int length=3;
+int count = 0;
+
+foreach  (string i in words)
+      
+   if ( i.Length > length )
+   {
+                
+   }
+   else
+   {
+    Console.WriteLine(i);
+    NS[count] = i;
+    count++;  
+
+   }
+     Console.Write("NS [");
+    foreach ( string ff in NS)
+    {
+       
+        Console.Write($" {ff}");
+        
+         
+    }
+    Console.Write("]");
+
+//////////////////////////////////////////////////////////////////////////
